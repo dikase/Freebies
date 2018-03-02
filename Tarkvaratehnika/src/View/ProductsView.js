ed2b38120744eb68ecf78ff044ac1cdb9cae47b4
Marion Tamm <tamm.marion@gmail.com>
2018-03-02 12:49:59 +0200

    Vaadete lisamine

---
+++ Tarkvaratehnika/src/View/ProductsView.js
@@ -0,0 +1,4 @@
+export class ProductsView{
+constructor(){
+	}
+}
--- Tarkvaratehnika/src/app.js
+++ Tarkvaratehnika/src/app.js
@@ -1,5 +1,16 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+constructor(){}
+ configureRouter(config, router)
+ {
+	 this.router=router;
+	 config.title="Freebies";
+	 
+	 config.map([
+	 {route: ['', 'View'], name:'View', moduleId: 'View/index'},
+	 {route: 'AddProductView', name:'AddProductView', moduleId:'View/AddProductView', nav:true},
+	 {route: 'ProductsView', name:'ProductsView', moduleId:'View/ProductsView', nav:true},
+	 {route: 'ProductView', name:'ProductView', moduleId:'View/ProductView', nav:true}
+	 ]);
+ 
+ }
 }
