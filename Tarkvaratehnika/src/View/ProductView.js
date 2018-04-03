a6d4a444ee7055205b597edf6dfcb240a20ebc3c
mariliisma <mariliis.marge@gmail.com>
2018-04-03 16:31:45 +0300

    ProductView täiendus

--- Tarkvaratehnika/src/View/ProductView.js
+++ Tarkvaratehnika/src/View/ProductView.js
@@ -1,4 +1,11 @@
+import {bindable, bindingMode} from 'aurelia-framework';
+
 export class ProductView{
-constructor(){
-	}
+  @bindable product = null;
+  in_wishlist = false;
+
+  change(elem){
+    this.in_wishlist = !this.in_wishlist;
+    alert(this.in_wishlist ? "Soov edastatud!" : "Soov tühistatud!");
+  }
 }
