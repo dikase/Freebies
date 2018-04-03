371c24646c6ba0832b28fb03f55bcbb88d224c16
dikase <dikase@ttu.ee>
2018-04-03 16:24:34 +0300

    ProductsView täiendus

--- Tarkvaratehnika/src/View/ProductsView.js
+++ Tarkvaratehnika/src/View/ProductsView.js
@@ -1,4 +1,19 @@
+import {HttpClient, json} from 'aurelia-fetch-client';
+import environment from "../environment";
+
 export class ProductsView{
-constructor(){
-	}
+  products = [];
+
+  constructor() {
+    this.httpClient = new HttpClient();
+  }
+  attached() {
+    this.load_products();
+  }
+
+  load_products(){
+    this.httpClient.fetch(environment.url + "getProducts")
+      .then(response => response.json())
+      .then(prs => this.products = prs);
+  }
 }
